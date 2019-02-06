e31534a90e7b89984cc23d5605f00e2af62b51cc
John Diehl <jr_diehl@baylor.edu>
2019-02-06 13:48:07 -0600

    Refactoring SecurityContext

--- src/main/java/edu/baylor/ecs/seer/common/context/SeerSecurityContext.java
+++ src/main/java/edu/baylor/ecs/seer/common/context/SeerSecurityContext.java
@@ -3,10 +3,7 @@ package edu.baylor.ecs.seer.common.context;
 import edu.baylor.ecs.seer.common.security.SecurityMethod;
 import edu.baylor.ecs.seer.common.security.SecurityRole;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @Author: JR
@@ -28,8 +25,58 @@ public class SeerSecurityContext {
     /**
      * from Local Weaver
      */
-    private Map<SecurityMethod, Set<SecurityRole>> roles = new HashMap<>();
+    private Set<SecurityMethod> securityMethods;
 
-    private Map<SecurityMethod, Set<SecurityMethod>> nodes = new HashMap<>();
+    public SeerSecurityContext(String securityRoleSpecification) {
+        this.securityRoleSpecification = securityRoleSpecification;
+    }
+
+    public String getSecurityRoleSpecification() {
+        return securityRoleSpecification;
+    }
+
+    public void setSecurityRoleSpecification(String securityRoleSpecification) {
+        this.securityRoleSpecification = securityRoleSpecification;
+    }
+
+    public List<String> getSecurityViolations() {
+        return securityViolations;
+    }
+
+    public void setSecurityViolations(List<String> securityViolations) {
+        this.securityViolations = securityViolations;
+    }
+
+    public Set<SecurityMethod> getSecurityMethods() {
+        return securityMethods;
+    }
+
+    public void setSecurityMethods(Set<SecurityMethod> securityMethods) {
+        this.securityMethods = securityMethods;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SeerSecurityContext that = (SeerSecurityContext) o;
+        return Objects.equals(securityRoleSpecification, that.securityRoleSpecification) &&
+                Objects.equals(securityViolations, that.securityViolations) &&
+                Objects.equals(securityMethods, that.securityMethods);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(securityRoleSpecification, securityViolations, securityMethods);
+    }
+
+    @Override
+    public String toString() {
+        return "SeerSecurityContext{" +
+                "securityRoleSpecification='" + securityRoleSpecification + '\'' +
+                ", securityViolations=" + securityViolations +
+                ", securityMethods=" + securityMethods +
+                '}';
+    }
 
 }
