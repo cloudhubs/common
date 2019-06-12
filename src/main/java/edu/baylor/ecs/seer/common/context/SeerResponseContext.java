07959158b847dd42b5f7b1c8a1cdafc79a2ff05f
Andrew Walker <Andrew_Walker2@baylor.edu>
2019-06-12 09:15:15 +0200

    SEER-59: Adds a getters/setters to SeerResponseContext

--- src/main/java/edu/baylor/ecs/seer/common/context/SeerResponseContext.java
+++ src/main/java/edu/baylor/ecs/seer/common/context/SeerResponseContext.java
@@ -18,5 +18,44 @@ public class SeerResponseContext {
     public SeerResponseContext(){
 
     }
-     
+
+    public Integer getModulesScanned() {
+        return modulesScanned;
+    }
+
+    public void setModulesScanned(Integer modulesScanned) {
+        this.modulesScanned = modulesScanned;
+    }
+
+    public Timestamp getRequestCompleted() {
+        return requestCompleted;
+    }
+
+    public void setRequestCompleted(Timestamp requestCompleted) {
+        this.requestCompleted = requestCompleted;
+    }
+
+    public List<String> getMicroservicesNames() {
+        return microservicesNames;
+    }
+
+    public void setMicroservicesNames(List<String> microservicesNames) {
+        this.microservicesNames = microservicesNames;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
